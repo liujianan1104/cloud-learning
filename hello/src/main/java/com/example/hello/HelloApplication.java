47a01af3474344a53e419308ffa8491d6f79b9c6
liujn <liujn@qdedu.cn>
2019-11-15 17:07:59 +0800

    增加apollo配置

--- hello/src/main/java/com/example/hello/HelloApplication.java
+++ hello/src/main/java/com/example/hello/HelloApplication.java
@@ -1,11 +1,13 @@
 package com.example.hello;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableApolloConfig
 public class HelloApplication {
 
     public static void main(String[] args) {
--- hello/src/main/java/com/example/hello/web/HelloController.java
+++ hello/src/main/java/com/example/hello/web/HelloController.java
@@ -1,13 +1,18 @@
 package com.example.hello.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${userId:100}")
+    private int userId;
     
     @RequestMapping("/hello")
     public String index() {
+        System.out.println(userId);
         return "hello1111";
     }
 }
